a1edefd3f4fb56e3b614479dab1b85189a66709e
Maria Bruner <mariab2898@gmail.com>
2021-09-30 19:44:17 -0400

    working on it

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -5,7 +5,7 @@ switch(window.location.hostname) {
         APIURL = 'http://localhost:3000'
         break
 
-    case 'meb-fight-game-app.herokuapp.com':
+    case 'meb-fight-game.herokuapp.com':
         APIURL = 'https://meb-fight-game-app.herokuapp.com'
         break
 }
