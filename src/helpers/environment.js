31e69ebb0accb35e8d86cbc36939e19d4fb71275
Maria Bruner <mariab2898@gmail.com>
2021-09-30 19:24:16 -0400

    trying again

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -6,7 +6,7 @@ switch(window.location.hostname) {
         break
 
     case 'meb-fight-game-app.herokuapp.com':
-        APIURL = 'https://meb-fight-game-app.herokuapp.com/'
+        APIURL = 'https://meb-fight-game-app.herokuapp.com'
         break
 }
 
